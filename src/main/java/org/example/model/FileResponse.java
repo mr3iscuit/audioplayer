9c7f01317f1a862c466fa72f6c5876a2ebb936ac
mr3iscuit <eyvaz.bayramov2018@gmail.com>
2024-09-03 03:05:12 +0400

    more readable

--- src/main/java/org/example/model/FileResponse.java
+++ src/main/java/org/example/model/FileResponse.java
@@ -2,39 +2,13 @@ package org.example.model;
 
 import lombok.*;
 
-import java.util.List;
-
-
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@Getter
+@Setter
 public class FileResponse {
     private Long fileSize;
     private String fileType;
     private Integer sampleRate;
-
-    // Getters and Setters
-    public Long getFileSize() {
-        return fileSize;
-    }
-
-    public void setFileSize(Long fileSize) {
-        this.fileSize = fileSize;
-    }
-
-    public String getFileType() {
-        return fileType;
-    }
-
-    public void setFileType(String fileType) {
-        this.fileType = fileType;
-    }
-
-    public Integer getSampleRate() {
-        return sampleRate;
-    }
-
-    public void setSampleRate(Integer sampleRate) {
-        this.sampleRate = sampleRate;
-    }
 }
